4d913867458a2ed95840f1584696a0def44493c3
boaTTTTTT <53853565+boaTTTTTT@users.noreply.github.com>
2020-10-06 23:38:58 +0700

    add work RPI

--- workRPI.js
+++ workRPI.js
@@ -1,6 +1,41 @@
-var http = require('http');
+const mongoose=require('mongoose');
+var express=require("express");
 
-http.createServer(function(req, res){
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end('hi hi hi hi hi hi');
-}).listen(3012);
+var app=express();
+mongoose.set('debug',true)
+mongoose.connect('mongodb://localhost/s60030089',{useNewUrlParser: true, useUnifiedTopology: true});
+const myModel=mongoose.model('Boat',new mongoose.Schema({name: String, surname: String, email : String, picUrl: String}));
+var query=myModel.where({email:'60030089@kmitl.ac.th'});
+app.use(express.static(__dirname + '/public'));
+app.get('/',function(req,res){
+    query.findOne((err, data)=>{
+        if(!err){
+            //res.json(data);
+            var html=
+                "<html>"+
+                "<title>My profile </title>"+
+                "<head>"+
+                "<link rel='stylesheet' type='text/css' href='RPI.css'>"+
+                "</head>"+
+                "<body>"+
+                "<b>Hello</b>"+
+                "<br></body>"+
+                "<a>"+data.name+" "+data.surname+"<br></a>"+
+                "<b>Contact Me<br></b>"+
+                "<a>Email : "+data.email+"<br></a>"+
+                "<img class='"+data.picUrl+"' src='./profile.jpg' >"+
+                "</html>";
+                res.send(html)
+        }
+    });
+    
+    
+    // res.send(html);
+
+});
+
+app.get('/myprofile',(req, res)=>{
+    res.send("This is myprofile page.");
+})
+
+app.listen(3012);
